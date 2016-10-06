424ac023be6ff13b98d5594e33c086d6220a35b9
Tim Oliver <me@timoliver.com.au>
2016-10-05 23:46:51 -0700

    Added text field and animation

--- TOSearchBar/TOSearchBar.h
+++ TOSearchBar/TOSearchBar.h
@@ -16,14 +16,20 @@ typedef NS_ENUM(NSInteger, TOSearchBarStyle) {
 IB_DESIGNABLE
 @interface TOSearchBar : UIControl
 
+/** The search text, entered by the user */
+@property (nonatomic, copy) IBInspectable NSString *text;
+
+/** The contents of the placeholder text. Defaults to 'Search' */
+@property (nonatomic, copy) IBInspectable NSString *placeholderText;
+
 /** How many points on each side the search bar is inset */
 @property (nonatomic, assign) IBInspectable CGFloat horizontalInset;
 
-/** The contents of the placeholder text. Defaults to 'Search' */
-@property (nonatomic, copy) IBInspectable NSString *placeholderString;
-
-
+/** Whether the text field is currently presenting the keyboard */
 @property (nonatomic, assign) BOOL editing;
+
+/** Whether the text field contains any text or not */
+@property (nonatomic, readonly) BOOL hasSearchText;
 
 /** The tint color of the placeholder content (Both the icon and the text) */
 @property (nonatomic, strong) IBInspectable UIColor *placeholderTintColor UI_APPEARANCE_SELECTOR;
@@ -31,7 +37,7 @@ IB_DESIGNABLE
 /** The tint color of the rounded background rectangle */
 @property (nonatomic, strong) IBInspectable UIColor *barBackgroundTintColor UI_APPEARANCE_SELECTOR;
 
-/** The tint color of the background when the user is editing the text field */
-@property (nonatomic, strong) IBInspectable UIColor *barBackgroundTintColorEditing UI_APPEARANCE_SELECTOR;
+/** The tint color of the background when the text field is selected */
+@property (nonatomic, strong) IBInspectable UIColor *barBackgroundTintColorHighlighted UI_APPEARANCE_SELECTOR;
 
 @end
--- TOSearchBar/TOSearchBar.m
+++ TOSearchBar/TOSearchBar.m
@@ -9,10 +9,14 @@
 #import "TOSearchBar.h"
 #import "TOSearchBar+Assets.h"
 
-@interface TOSearchBar () <UIGestureRecognizerDelegate>
+static const CGFloat kTOSearchBarInset = 8.0f; // inset from inside the bar
+static const CGFloat kTOSearchBarIconMargin = 5.0f; // spacing between icon and placeholder
+
+@interface TOSearchBar () <UIGestureRecognizerDelegate, UITextFieldDelegate>
 
 // UI components
 @property (nonatomic, strong) UIImageView *barBackgroundView;
+@property (nonatomic, strong) UIView *containerView;
 @property (nonatomic, strong) UILabel *placeholderLabel;
 @property (nonatomic, strong) UITextField *searchTextField;
 @property (nonatomic, strong) UIButton *cancelButton;
@@ -23,19 +27,21 @@
 
 /* View Set-up */
 - (void)setUpViews;
-- (void)setUpBackgroundView;
+- (void)setUpBackgroundViews;
 - (void)setUpPlaceholderViews;
 - (void)setUpButtons;
+- (void)setUpTextField;
 - (void)setUpGestureRecognizers;
 
-/* View Animating */
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated;
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated;
+
+- (void)textFieldDidChange:(UITextField *)textField;
 
 @end
 
 @implementation TOSearchBar
 
-@synthesize barBackgroundTintColor            = _barBackgroundTintColor;
+@synthesize barBackgroundTintColor = _barBackgroundTintColor;
 
 #pragma mark - View Lifecycle -
 - (void)didMoveToSuperview
@@ -49,13 +55,14 @@
 #pragma mark - View Set-up -
 - (void)setUpViews
 {
-    [self setUpBackgroundView];
+    [self setUpBackgroundViews];
     [self setUpPlaceholderViews];
     [self setUpButtons];
+    [self setUpTextField];
     [self setUpGestureRecognizers];
 }
 
-- (void)setUpBackgroundView
+- (void)setUpBackgroundViews
 {
     if (self.barBackgroundView) {
         return;
@@ -66,6 +73,14 @@
     UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleWidth;
     self.barBackgroundView.tintColor = self.barBackgroundTintColor;
     [self addSubview:self.barBackgroundView];
+    
+    if (self.containerView) {
+        return;
+    }
+    
+    self.containerView = [[UIView alloc] initWithFrame:self.barBackgroundView.frame];
+    self.containerView.autoresizingMask = self.barBackgroundView.autoresizingMask;
+    [self addSubview:self.containerView];
 }
 
 - (void)setUpPlaceholderViews
@@ -80,7 +95,7 @@
     self.placeholderLabel.text = @"Search";
     self.placeholderLabel.textColor = [UIColor grayColor];
     [self.placeholderLabel sizeToFit];
-    [self addSubview:self.placeholderLabel];
+    [self.containerView addSubview:self.placeholderLabel];
 
     // Set up the accessory icon view
     if (self.iconView) {
@@ -89,7 +104,23 @@
 
     self.iconView = [[UIImageView alloc] initWithImage:[TOSearchBar sharedSearchIcon]];
     self.iconView.tintColor = [UIColor grayColor];
-    [self addSubview:self.iconView];
+    [self.containerView addSubview:self.iconView];
+}
+
+- (void)setUpTextField
+{
+    if (self.searchTextField) {
+        return;
+    }
+    
+    self.searchTextField = [[UITextField alloc] initWithFrame:self.containerView.bounds];
+    self.searchTextField.font = [UIFont systemFontOfSize:15.0f];
+    self.searchTextField.textColor = [UIColor blackColor];
+    self.searchTextField.delegate = self;
+    self.searchTextField.returnKeyType = UIReturnKeySearch;
+    [self.containerView addSubview:self.searchTextField];
+    
+    [self.searchTextField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
 }
 
 - (void)setUpButtons
@@ -105,6 +136,7 @@
     
     // A long-press recognizer is used in order to detect when the user initially touches the glass
     self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapGestureRecognized:)];
+    self.tapGestureRecognizer.delegate = self;
     [self addGestureRecognizer:self.tapGestureRecognizer];
 }
 
@@ -114,32 +146,113 @@
 {
     CGRect frame;
     
+    // Layout the background view (and content container)
     frame = self.barBackgroundView.frame;
     frame.size.width = (self.frame.size.width) - (self.horizontalInset * 2.0f);
     frame.origin.x = self.horizontalInset;
     frame.origin.y = floorf((self.frame.size.height - frame.size.height) * 0.5f);
     self.barBackgroundView.frame = frame;
+    self.containerView.frame = frame;
     
+    // layout the place holder label
     frame = self.placeholderLabel.frame;
-    frame.origin.x = CGRectGetMidX(self.barBackgroundView.frame) - (CGRectGetWidth(frame) * 0.5f);
-    frame.origin.y = CGRectGetMidY(self.barBackgroundView.frame) - (CGRectGetHeight(frame) * 0.5f);
+    if (self.editing || self.hasSearchText) {
+        frame.origin.x = (kTOSearchBarIconMargin + kTOSearchBarInset) + self.iconView.frame.size.width;
+    }
+    else {
+        frame.origin.x = floorf((CGRectGetWidth(self.containerView.frame) - CGRectGetWidth(frame)) * 0.5f);
+    }
+    frame.origin.y = floorf((CGRectGetHeight(self.containerView.frame) - CGRectGetHeight(frame)) * 0.5f);
     self.placeholderLabel.frame = frame;
     
+    // layout the icon
     frame = self.iconView.frame;
-    frame.origin.x = CGRectGetMinX(self.placeholderLabel.frame) - (CGRectGetWidth(self.iconView.frame) + 5);
+    if (self.editing || self.hasSearchText) {
+        frame.origin.x = kTOSearchBarInset;
+    }
+    else {
+        frame.origin.x = CGRectGetMinX(self.placeholderLabel.frame) - (CGRectGetWidth(self.iconView.frame) + kTOSearchBarIconMargin);
+    }
     frame.origin.y = CGRectGetMidY(self.placeholderLabel.frame) - (CGRectGetHeight(self.iconView.frame) * 0.5f);
     self.iconView.frame = frame;
+    
+    // lay out the text field
+    frame = self.searchTextField.frame;
+    frame.size = self.containerView.frame.size;
+    frame.size.width = CGRectGetWidth(frame) - self.placeholderLabel.frame.origin.x;
+    frame.origin.x = self.placeholderLabel.frame.origin.x;
+    frame.origin.y = 0.0f;
+    self.searchTextField.frame = frame;
 }
 
 #pragma mark - Event Handling -
 - (void)tapGestureRecognized:(UITapGestureRecognizer *)recognizer
 {
-    NSLog(@"XD");
+    [self.searchTextField becomeFirstResponder];
+    [self setEditing:YES animated:YES];
 }
 
-#pragma mark - Animation -
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    return !(self.searchTextField.isFirstResponder);
+}
+
+- (BOOL)resignFirstResponder
+{
+    [self.searchTextField resignFirstResponder];
+    [self setEditing:NO animated:YES];
+    return [super resignFirstResponder];
+}
+
+#pragma mark - Editing -
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated
+{
+    self.editing = editing;
+    
+    if (animated == NO) {
+        return;
+    }
+    
+    [UIView animateWithDuration:0.4f
+                          delay:0.0f
+         usingSpringWithDamping:1.0f
+          initialSpringVelocity:0.1f
+                        options:0
+                     animations:
+     ^{
+        [self layoutIfNeeded];
+     } completion:nil];
+}
+
+#pragma mark - Text Field Delegate -
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [self setEditing:YES animated:YES];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [textField layoutIfNeeded]; // Necessary to stop bouncing animation glitch
+    [self setEditing:NO animated:YES];
+}
+
+- (void)textFieldDidChange:(UITextField *)textField
+{
+    self.placeholderLabel.hidden = textField.text.length > 0;
+}
 
 #pragma mark - Accessors -
+- (void)setEditing:(BOOL)editing
+{
+    _editing = editing;
+    [self setNeedsLayout];
+}
+
+- (BOOL)hasSearchText
+{
+    return self.searchTextField.text.length > 0;
+}
+
 - (UIColor *)barBackgroundTintColor
 {
     if (_barBackgroundTintColor == nil) {
